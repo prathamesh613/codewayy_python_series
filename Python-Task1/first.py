c8912ace4c874e45417572558f46eee1bb01b73f
prathamesh613 <prathmesh613@gmail.com>
2020-06-11 08:25:56 +0530

    Initial Commit First.py

---
+++ Python-Task1/first.py
@@ -0,0 +1,53 @@
+print("=" * 80) # Asthetics
+# Operations on Numbers
+class Numbers:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        # Assign num1 and num2
+    #Addition
+    def add(self):
+        return num1 + num2
+    #Subtraction
+    def sub(self):
+        return num1 - num2
+    #Subtraction with absolute value
+    def sub_abs(self):
+        return abs(num1 - num2)
+    #Multiplication
+    def mul(self):
+        return num1 * num2
+    #Division
+    def div(self):
+        return (num1/num2)
+    #Division with floor value
+    def div_floor(self):
+        return num1//num2
+    #Exponent
+    def pow(self):
+        return num1 ** num2
+
+#Integers
+print("Integers".upper())
+
+num1, num2 = (int(x) for x in input("Enter Two Integers : ").split())
+# Input 2 Numbers and perform operations
+Int = Numbers(num1, num2)
+integer_operations = {'Addition' : Int.add(), 'Subtraction' : Int.sub(), 'Absolute_Subtracion' : Int.sub_abs(),
+                'Multiplication' : Int.mul(), 'Division' : Int.div(), 'Floor_Division' : Int.div_floor(),
+                'Power' : Int.pow()}
+# print(help({})) #Taking Help from Dictionary Class
+# Print All Integer Operations
+print(integer_operations)
+print("=" * 80) # Asthetics
+
+#Floating Point Numbers
+print("Floating Point Numbers".upper())
+
+num1, num2 = (float(x) for x in input("Enter Two Floating Point Numbers : ").split())
+Float = Numbers(num1, num2)
+floating_operations = {'Addition' : Float.add(), 'Subtraction' : Float.sub(), 'Absolute_Subtracion' : Float.sub_abs(),
+                'Multiplication' : Int.mul(), 'Division' : Float.div(), 'Floor_Division' : Float.div_floor(),
+                'Power' : Float.pow()}
+print(floating_operations)
+print("=" * 80) # Asthetics
---
+++ README.md
@@ -0,0 +1,11 @@
+# **Codewayy** Python Series - ***Day 1***
+# Python-Task1
+## First.py
+> Write a program in Python which would include some code working with integers and float variables
+
+## Second.py
+> Write a program in Python which should display your following details :
+  1. Full Name (String Concatenation should be used)
+  2. College Name with Address (Use String Concatenation)
+  3. Initialize marks of 5 different subjects with good variable names.
+  4. Calculate and display the total marks, percentage
